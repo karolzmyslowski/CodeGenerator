6f5c33c20496351e153f56e100436b5b5209f159
karolzmyslowski <33572871+karolzmyslowski@users.noreply.github.com>
2018-03-11 20:06:41 +0100

    Initial Commit

---
+++ QRGenerate/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  QRGenerate
+//
+//  Created by Karol Zmyslowski on 11.03.2018.
+//  Copyright © 2018 Karol Zmyslowski. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
