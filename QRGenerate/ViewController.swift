ddae7c937cdd6a81c3d012bd7e67fdf35a7f6b7a
karolzmyslowski <33572871+karolzmyslowski@users.noreply.github.com>
2018-03-11 20:48:00 +0100

    1.0

--- QRGenerate/ViewController.swift
+++ QRGenerate/ViewController.swift
@@ -10,6 +10,11 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var textField: UITextField!
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var segment: UISegmentedControl!
+    
+    var filter: CIFilter!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +25,25 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func buttonTapped(_ sender: Any) {
+        
+        if let text = textField.text{
+            
+            let data = text.data(using: .ascii, allowLossyConversion: false)
+            
+            if segment.selectedSegmentIndex == 0 {
+                filter = CIFilter(name: "CICode128BarcodeGenerator")
+            } else {
+                filter = CIFilter(name: "CIQRCodeGenerator")
+            }
+           
+            filter?.setValue(data, forKey: "inputMessage")
+            let transform = CGAffineTransform(scaleX: 10, y: 10)
+            let img = UIImage(ciImage: filter.outputImage!.transformed(by: transform))
+            
+            imageView.image = img
+        }
+    }
+    
 }
 
